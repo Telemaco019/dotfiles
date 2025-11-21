959567c5cb9f278b323f2973f930d3a043f03f8a
Michele Zanotti <m.zanotti@nebuly.ai>
2025-11-21 11:02:36 +0000

    add copilot config

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,33 @@
 return {
-	"github/copilot.vim",
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot", -- load when you run :Copilot
+		event = "InsertEnter", -- or when you start typing
+		build = ":Copilot auth", -- helper to run auth once
+		opts = {
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				keymap = {
+					accept = "<M-l>", -- Alt+l to accept
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+			},
+			panel = {
+				enabled = false,
+			},
+		},
+		config = function(_, opts)
+			require("copilot").setup(opts) -- this is the important part
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		dependencies = { "copilot.lua" },
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
 }
