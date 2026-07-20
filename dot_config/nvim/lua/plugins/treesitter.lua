a71fad8db476310323086bf1776cbde3f40e9059
Michele Zanotti <m.zanotti019@gmail.com>
2026-07-20 10:37:53 +0200

    update

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = "master",
+		branch = "main",
 		build = ":TSUpdate",
 		lazy = false,
 		dependencies = {
@@ -15,28 +15,26 @@ return {
 			},
 		},
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"python",
-					"yaml",
-					"terraform",
-					"hcl",
-					"bash",
-					"go",
-					"lua",
-					"zig",
-					"dockerfile",
-					"html",
-					"css",
-					"json",
-					"markdown",
-					"markdown_inline",
-					"vim",
-					"vimdoc",
-				},
-				highlight = { enable = true },
-				indent = { enable = true },
-				auto_install = true,
+			require("nvim-treesitter").setup({})
+
+			require("nvim-treesitter").install({
+				"python", "yaml", "terraform", "hcl", "bash", "go", "lua", "zig",
+				"dockerfile", "html", "css", "json", "markdown", "markdown_inline",
+				"vim", "vimdoc",
+			})
+
+			-- Enable treesitter-driven highlighting/folding/indentation per buffer.
+			-- pcall guards buffers whose language has no parser installed yet.
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("treesitter-start", { clear = true }),
+				callback = function(args)
+					pcall(function()
+						vim.treesitter.start(args.buf)
+						vim.wo[args.win].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+						vim.wo[args.win].foldmethod = "expr"
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end)
+				end,
 			})
 		end,
 	},
